9754b0098d54fb15f8b102dc788a87cc5ed57ee7
kulala <y.kurabayashi1031@gmail.com>
2019-07-20 18:37:50 +0000

    add status

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,7 +42,13 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request,[
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+            ]);
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         return redirect('/');
@@ -85,7 +91,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request,[
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+            ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
